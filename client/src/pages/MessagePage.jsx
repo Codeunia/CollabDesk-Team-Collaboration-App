daad9dced255489848055cfc14dd2c0f8406b079
pallab-at-tech <pallab861774@gmail.com>
2025-08-15 22:46:15 +0530

    ui fixed

--- client/src/pages/MessagePage.jsx
+++ client/src/pages/MessagePage.jsx
@@ -227,9 +227,9 @@ const MessagePage = () => {
 
 
     return (
-        <section className='h-[calc(100vh-60px)] w-full grid grid-rows-[65px_1fr_55px] overflow-hidden'>
+        <section className='h-[calc(100vh-60px)] w-full overflow-hidden'>
 
-            <div className='bg-[#21222b] px-4 grid grid-cols-[300px_1fr] w-full items-center text-white shadow-md shadow-[#57575765]'>
+            <div className='bg-[#21222b] fixed top-0 z-50 px-4 grid grid-cols-[300px_1fr] w-full items-center text-white shadow-md shadow-[#57575765]'>
 
                 <div className={`flex gap-2.5 ${location?.allMessageDetails?.group_type === "GROUP" ? "items-center" : "items-start"} pl-2`}>
                     <div className='flex items-center justify-center'>
@@ -256,164 +256,167 @@ const MessagePage = () => {
                 </div>
             </div>
 
-            <div className='h-full overflow-y-auto px-2.5 flex flex-col gap-2.5 py-4 chat-scrollbar relative' style={{ willChange: 'transform' }}>
-                {
-                    Array.isArray(messages) && messages.map((value, index) => {
+            <div className='grid grid-rows-[1fr_55px] h-full'>
 
-                        const isSelfMessage = value?.senderId?._id === user?._id || value?.senderId === user?._id
-                        const date = new Date(value?.createdAt)
-                        const indianTime = date.toLocaleString("en-IN", {
-                            timeZone: "Asia/Kolkata",
-                            hour: "2-digit",
-                            minute: "2-digit",
-                            year: "numeric",
-                            month: "2-digit",
-                            day: "2-digit"
-                        })
-
-
-                        return (
-                            <div key={`new key-${index}`} className={`bg-[#f1f1f1] w-fit text-base rounded text-blue-950  px-1 py-0.5 ${location?.allMessageDetails?.group_type === "GROUP" && index === 0 ? "self-center" : `${isSelfMessage ? "self-end" : "self-start"}`} `}>
-
-                                {
-                                    (index !== 0 && user?._id && value?.senderId !== user?._id) && (
-                                        isGroup && <div className='sm:-mb-0.5 -mb-1.5 text-sm'>
-                                            {value?.senderName}
-                                        </div>
-
-                                    )
-                                }
-
-                                <div className='pt-1 r'>
-                                    {
-                                        value?.image && <img src={value?.image} alt="" className='w-[200px]' />
-                                    }
-
-                                    {
-                                        value?.video && <video src={value?.video} controls className='w-[200px]'></video>
-                                    }
-
-                                    {value?.other_fileUrl_or_external_link && (
-                                        <button
-                                            onClick={() => window.open(value.other_fileUrl_or_external_link, "_blank")}
-                                            className="bg-blue-500 text-white px-3 py-1 rounded"
-                                        >
-                                            Open PDF
-                                        </button>
-                                    )}
-                                </div>
-                                <p className={`${(location?.allMessageDetails?.group_type === "GROUP" && index === 0) ? "text-[13px]" : "font-semibold"} `}>
-                                    {value?.text}
-                                </p>
-
-                                <p className={`text-sm opacity-[60%] ${isSelfMessage ? "text-end" : "text-start"} `}>
-                                    {indianTime}
-                                </p>
-
-                                <div ref={messagesEndRef} />
-                            </div>
-                        )
-                    })
-                }
-            </div>
-
-
-            <div className='bg-[#1f2029] w-full rounded-t-md grid grid-cols-[100px_1fr_100px] items-center text-white shadow-md shadow-[#154174]'>
-
-                <div className='flex items-center justify-center relative'>
-                    <MdAttachment size={29} onClick={() => setOpenAttach(true)} className='cursor-pointer' />
-
+                <div className='h-full overflow-y-auto px-2.5 flex flex-col gap-2.5 py-4 chat-scrollbar relative' style={{ willChange: 'transform' }}>
                     {
-                        openAttach && (
-                            <div ref={attachRef} className='bg-[#f1f1f1] text-blue-950 absolute bottom-10 left-8 min-h-[110px] min-w-[100px] flex flex-col items-start px-4 gap-2 py-2 rounded-t-2xl rounded-r-2xl rounded-l-md'>
+                        Array.isArray(messages) && messages.map((value, index) => {
 
-                                <div className='flex gap-4 items-center justify-center'>
+                            const isSelfMessage = value?.senderId?._id === user?._id || value?.senderId === user?._id
+                            const date = new Date(value?.createdAt)
+                            const indianTime = date.toLocaleString("en-IN", {
+                                timeZone: "Asia/Kolkata",
+                                hour: "2-digit",
+                                minute: "2-digit",
+                                year: "numeric",
+                                month: "2-digit",
+                                day: "2-digit"
+                            })
 
-                                    <div onClick={() => {
-                                        imgRef.current.click()
-                                    }}
-                                        className='cursor-pointer'
-                                    >
-                                        <IoImage size={42} />
-                                        <p >image</p>
-                                        <input ref={imgRef} onChange={handleAllAtachFile} type="file" accept="image/*" name='image' hidden />
+
+                            return (
+                                <div key={`new key-${index}`} className={`bg-[#f1f1f1] w-fit text-base rounded text-blue-950  px-1 py-0.5 ${location?.allMessageDetails?.group_type === "GROUP" && index === 0 ? "self-center" : `${isSelfMessage ? "self-end" : "self-start"}`} `}>
+
+                                    {
+                                        (index !== 0 && user?._id && value?.senderId !== user?._id) && (
+                                            isGroup && <div className='sm:-mb-0.5 -mb-1.5 text-sm'>
+                                                {value?.senderName}
+                                            </div>
+
+                                        )
+                                    }
+
+                                    <div className='pt-1 r'>
+                                        {
+                                            value?.image && <img src={value?.image} alt="" className='w-[200px]' />
+                                        }
+
+                                        {
+                                            value?.video && <video src={value?.video} controls className='w-[200px]'></video>
+                                        }
+
+                                        {value?.other_fileUrl_or_external_link && (
+                                            <button
+                                                onClick={() => window.open(value.other_fileUrl_or_external_link, "_blank")}
+                                                className="bg-blue-500 text-white px-3 py-1 rounded"
+                                            >
+                                                Open PDF
+                                            </button>
+                                        )}
                                     </div>
+                                    <p className={`${(location?.allMessageDetails?.group_type === "GROUP" && index === 0) ? "text-[13px]" : "font-semibold"} `}>
+                                        {value?.text}
+                                    </p>
 
-                                    <div onClick={() => videoRef.current.click()}
-                                        className='cursor-pointer'
-                                    >
-                                        <RiFolderVideoFill size={42} />
-                                        <p className='text-center'>video</p>
-                                        <input type="file" ref={videoRef} onChange={handleAllAtachFile} accept="video/*" name='video' hidden />
-                                    </div>
+                                    <p className={`text-sm opacity-[60%] ${isSelfMessage ? "text-end" : "text-start"} `}>
+                                        {indianTime}
+                                    </p>
+
+                                    <div ref={messagesEndRef} />
                                 </div>
-
-                                <div className='flex gap-4 items-start justify-center'>
-                                    <div onClick={() => fileUrlRef.current.click()} className='cursor-pointer'>
-                                        <FaFileAlt size={42} />
-                                        <p className='text-center'>file</p>
-                                        <input type="file" ref={fileUrlRef} onChange={handleAllAtachFile} accept="application/pdf" name='other_fileUrl_or_external_link' hidden />
-                                    </div>
-
-                                    {/* <div className='cursor-pointer'>
-                                        <HiOutlineUserGroup size={42}/>
-                                        <p className='leading-[1] text-center'>Create group</p>
-                                    </div> */}
-                                </div>
-                            </div>
-                        )
+                            )
+                        })
                     }
                 </div>
 
+                <div className='bg-[#1f2029] w-full rounded-t-md grid grid-cols-[100px_1fr_100px] items-center text-white shadow-md shadow-[#154174]'>
 
-                {
-                    location?.allMessageDetails?.group_type === "PRIVATE" ? (
-                        <>
+                    <div className='flex items-center justify-center relative'>
+                        <MdAttachment size={29} onClick={() => setOpenAttach(true)} className='cursor-pointer' />
 
-                            <div>
-                                <input type="text" value={messageText}
+                        {
+                            openAttach && (
+                                <div ref={attachRef} className='bg-[#f1f1f1] text-blue-950 absolute bottom-10 left-8 min-h-[110px] min-w-[100px] flex flex-col items-start px-4 gap-2 py-2 rounded-t-2xl rounded-r-2xl rounded-l-md'>
 
-                                    onChange={e => setMessageText(e.target.value)}
+                                    <div className='flex gap-4 items-center justify-center'>
 
-                                    onKeyDown={(e) => {
-                                        if (e.key === "Enter" && !e.shiftKey) {
-                                            e.preventDefault();
-                                            handleOnClick();
-                                        }
-                                    }}
+                                        <div onClick={() => {
+                                            imgRef.current.click()
+                                        }}
+                                            className='cursor-pointer'
+                                        >
+                                            <IoImage size={42} />
+                                            <p >image</p>
+                                            <input ref={imgRef} onChange={handleAllAtachFile} type="file" accept="image/*" name='image' hidden />
+                                        </div>
 
-                                    className='w-full text-[#f3f3f3] outline-none' placeholder='Type a message...'
-                                />
-                            </div>
+                                        <div onClick={() => videoRef.current.click()}
+                                            className='cursor-pointer'
+                                        >
+                                            <RiFolderVideoFill size={42} />
+                                            <p className='text-center'>video</p>
+                                            <input type="file" ref={videoRef} onChange={handleAllAtachFile} accept="video/*" name='video' hidden />
+                                        </div>
+                                    </div>
 
-                            <div className='flex items-center justify-center cursor-pointer'>
-                                <IoSendOutline size={29} onClick={() => handleOnClick()} />
-                            </div>
-                        </>
-                    ) : (
-                        <>
-                            <div>
-                                <input type="text" value={messageText}
+                                    <div className='flex gap-4 items-start justify-center'>
+                                        <div onClick={() => fileUrlRef.current.click()} className='cursor-pointer'>
+                                            <FaFileAlt size={42} />
+                                            <p className='text-center'>file</p>
+                                            <input type="file" ref={fileUrlRef} onChange={handleAllAtachFile} accept="application/pdf" name='other_fileUrl_or_external_link' hidden />
+                                        </div>
 
-                                    onChange={e => setMessageText(e.target.value)}
+                                        {/* <div className='cursor-pointer'>
+                                        <HiOutlineUserGroup size={42}/>
+                                        <p className='leading-[1] text-center'>Create group</p>
+                                    </div> */}
+                                    </div>
+                                </div>
+                            )
+                        }
+                    </div>
 
-                                    onKeyDown={(e) => {
-                                        if (e.key === "Enter" && !e.shiftKey) {
-                                            e.preventDefault();
-                                            handleGroupMessageSend();
-                                        }
-                                    }}
 
-                                    className='w-full text-[#f3f3f3] outline-none' placeholder='Type a message...'
-                                />
-                            </div>
+                    {
+                        location?.allMessageDetails?.group_type === "PRIVATE" ? (
+                            <>
 
-                            <div className='flex items-center justify-center cursor-pointer'>
-                                <IoSendOutline size={29} onClick={() => handleGroupMessageSend()} />
-                            </div>
-                        </>
-                    )
-                }
+                                <div>
+                                    <input type="text" value={messageText}
+
+                                        onChange={e => setMessageText(e.target.value)}
+
+                                        onKeyDown={(e) => {
+                                            if (e.key === "Enter" && !e.shiftKey) {
+                                                e.preventDefault();
+                                                handleOnClick();
+                                            }
+                                        }}
+
+                                        className='w-full text-[#f3f3f3] outline-none' placeholder='Type a message...'
+                                    />
+                                </div>
+
+                                <div className='flex items-center justify-center cursor-pointer'>
+                                    <IoSendOutline size={29} onClick={() => handleOnClick()} />
+                                </div>
+                            </>
+                        ) : (
+                            <>
+                                <div>
+                                    <input type="text" value={messageText}
+
+                                        onChange={e => setMessageText(e.target.value)}
+
+                                        onKeyDown={(e) => {
+                                            if (e.key === "Enter" && !e.shiftKey) {
+                                                e.preventDefault();
+                                                handleGroupMessageSend();
+                                            }
+                                        }}
+
+                                        className='w-full text-[#f3f3f3] outline-none' placeholder='Type a message...'
+                                    />
+                                </div>
+
+                                <div className='flex items-center justify-center cursor-pointer'>
+                                    <IoSendOutline size={29} onClick={() => handleGroupMessageSend()} />
+                                </div>
+                            </>
+                        )
+                    }
+
+                </div>
 
             </div>
 
